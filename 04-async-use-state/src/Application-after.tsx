55f7419a6537c3087c639592a22803ce74dc2997
Klaus <whynotmemylove99@gmail.com>
2023-08-31 17:14:37 +0700

    add lesson 4,5 : effect and async

---
+++ 04-async-use-state/src/Application-after.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+// hover underlined items for information
+import { HeroType, fetchHero } from './heroes';
+import { HeroInformation } from './HeroInformation';
+
+//async is a keyword that you put in front of a function declaration to turn it into an async function
+//async function is a function that knows how to expect the possibility of the await keyword being used to invoke asynchronous code
+// https://viblo.asia/p/tim-hieu-ve-synchronous-va-asynchronous-trong-javascript-3Q75w7ze5Wb
+const Application = () => {
+  const [hero, setHero] = React.useState<HeroType | null>(null);
+
+  React.useEffect(() => {
+    fetchHero().then(h => setHero(h));
+  }, []);
+
+  return <main>
+    {hero && <HeroInformation hero={hero} />}
+  </main>;
+};
+
+export default Application;
---
+++ 05-typing-use-effect/src/Application-before.tsx
@@ -0,0 +1,20 @@
+//task: try to fix this code to make it work
+
+import { useEffect, useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => setTimeout(() => setCount(count + 1), 1000), [count]);
+
+  return (
+    <main>
+      <h1>{count}</h1>
+    </main>
+  );
+};
+
+const Application = () => <Counter />;
+
+export default Application;
+
